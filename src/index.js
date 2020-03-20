6e083012d244a541830a0c4df172ba0e4cbb0de2
AnhelinaVr <gelina242@gmail.com>
2020-03-20 18:29:50 +0300

    init: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!Array.isArray(array) || array.length == 0) return 0;
+    return Math.min.apply(null, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!Array.isArray(array) || array.length == 0) return 0;
+    return Math.max.apply(null, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || array.length == 0) return 0;
+    let res = 0;
+    for (i = 0; i < array.length; i++) {
+        res += array[i];
+    }
+    return (res / array.length);
 }
